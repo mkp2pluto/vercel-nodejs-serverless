339eb7919de9e501cb56a4a664bffd93a62ab626
mkp2pluto <>
2024-04-22 18:01:44 -0400

    new endpoints test

--- api/hello.ts
+++
@@ -1,4 +0,0 @@
-import type { VercelRequest, VercelResponse } from '@vercel/node'
-export default function handler(req: VercelRequest, res: VercelResponse) {
-  return res.json({req: req.req,query, res:"wip"});
-}
